89d6dfbdc915d9f9b7b754362446400675379452
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2022-09-23 14:52:21 +0200

    Optimize `segment_matmul` via `gemm_batched`

--- pyg_lib/csrc/ops/cpu/matmul_kernel.cpp
+++ pyg_lib/csrc/ops/cpu/matmul_kernel.cpp
@@ -1,4 +1,5 @@
 #include <ATen/ATen.h>
+#include <ATen/native/CPUBlas.h>
 #include <torch/library.h>
 
 #include "pyg_lib/csrc/utils/convert.h"
@@ -39,16 +40,56 @@ std::vector<at::Tensor> grouped_matmul_kernel(
 at::Tensor segment_matmul_kernel(const at::Tensor& input,
                                  const at::Tensor& ptr,
                                  const at::Tensor& other) {
+  const auto input_contig = input.contiguous();
+  const auto other_contig = other.contiguous();
   const auto size = pyg::utils::size_from_ptr(ptr).cpu();
-  const auto sizes = at::IntArrayRef(size.data_ptr<int64_t>(), size.numel());
   const auto out = input.new_empty({input.size(0), other.size(-1)});
-  auto out_parts = out.split_with_sizes(/*split_size=*/sizes, /*dim=*/0);
-  for (auto& out_part : out_parts)
-    out_part.resize_(0);
 
-  grouped_matmul_out_kernel(
-      input.contiguous().split_with_sizes(/*split_size=*/sizes, /*dim=*/0),
-      other.contiguous().split(/*split_size=*/1, /*dim=*/0), out_parts);
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX_AND2(
+      at::ScalarType::Half, at::ScalarType::BFloat16, input.scalar_type(),
+      "segment_matmul_kernel", [&] {
+        const auto input_data = input_contig.data_ptr<scalar_t>();
+        const auto other_data = other_contig.data_ptr<scalar_t>();
+        const auto size_data = size.data_ptr<int64_t>();
+        auto out_data = out.data_ptr<scalar_t>();
+
+        const auto K = input_contig.size(-1);
+        const auto N = other_contig.size(-1);
+        const auto B = size.size(0);
+        std::unordered_map<int64_t, std::vector<int64_t>> groups;
+        for (int64_t i = 0; i < B; ++i) {
+          const auto M_i = size_data[i];
+          if (groups.count(M_i))
+            groups[M_i].push_back(i);
+          else
+            groups.insert({M_i, {i}});
+        }
+
+        std::vector<int64_t> m_row_offsets(B + 1, 0);
+        std::partial_sum(size_data, size_data + B, m_row_offsets.begin() + 1);
+
+        for (const auto& group_pair : groups) {
+          const auto M_i = group_pair.first;
+          const auto& indices = group_pair.second;
+          const auto bs = indices.size();
+          std::vector<const scalar_t*> input_ptrs(bs);
+          std::vector<const scalar_t*> other_ptrs(bs);
+          std::vector<scalar_t*> out_ptrs(bs);
+          for (size_t i = 0; i < bs; ++i) {
+            input_ptrs[i] = input_data + (m_row_offsets[indices[i]] * K);
+            other_ptrs[i] = other_data + (indices[i] * K * N);
+            out_ptrs[i] = out_data + (m_row_offsets[indices[i]] * N);
+          }
+          const scalar_t** input_ptr = input_ptrs.data();
+          const scalar_t** other_ptr = other_ptrs.data();
+          scalar_t** out_ptr = out_ptrs.data();
+          at::native::cpublas::gemm_batched<scalar_t>(
+              at::native::TransposeType::NoTranspose,
+              at::native::TransposeType::NoTranspose, bs, M_i, N, K,
+              static_cast<scalar_t>(1), input_ptr, M_i, other_ptr, K,
+              static_cast<scalar_t>(0), out_ptr, M_i);
+        }
+      });
 
   return out;
 }
