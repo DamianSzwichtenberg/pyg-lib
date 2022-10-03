c09f14fe34c43ed83aeefcc59c3d484b9bbb6017
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2022-10-03 15:10:11 +0200

    CDF inner-loop: vectorize using at::vec::Vectorized

--- pyg_lib/csrc/random/cpu/biased_sampling.cpp
+++ pyg_lib/csrc/random/cpu/biased_sampling.cpp
@@ -1,6 +1,8 @@
 #include "pyg_lib/csrc/random/cpu/biased_sampling.h"
 
 #include <ATen/Parallel.h>
+#include <ATen/cpu/vec/functional.h>
+#include <ATen/cpu/vec/vec.h>
 
 namespace pyg {
 namespace random {
@@ -36,35 +38,40 @@ void biased_to_cdf_helper(int64_t* rowptr_data,
                           size_t rowptr_size,
                           const scalar_t* bias,
                           scalar_t* cdf) {
-  at::parallel_for(0, rowptr_size - 1, at::internal::GRAIN_SIZE / rowptr_size,
-                   [&](size_t _s, size_t _e) {
-                     // CDF conversion for each row
-                     for (size_t i = _s; i < _e; i++) {
-                       const scalar_t* beg = bias + rowptr_data[i];
-                       size_t len = rowptr_data[i + 1] - rowptr_data[i];
-                       scalar_t* out_beg = cdf + rowptr_data[i];
+  using Vec = at::vec::Vectorized<scalar_t>;
 
-                       // Remember sum and current element to
-                       // enable the in-place option (bias == cdf).
-                       scalar_t sum = 0;
-                       size_t j_;
-#ifdef _OPENMP
-#pragma omp simd reduction(+ : sum) linear(j_ : 1)
-#endif
-                       for (j_ = 0; j_ < len; j_++) {
-                         sum += beg[j_];
-                       }
+  at::parallel_for(
+      0, rowptr_size - 1, at::internal::GRAIN_SIZE / rowptr_size,
+      [&](int64_t _s, int64_t _e) {
+        // CDF conversion for each row
+        for (int64_t i = _s; i < _e; i++) {
+          const scalar_t* in_beg = bias + rowptr_data[i];
+          scalar_t* out_beg = cdf + rowptr_data[i];
+          int64_t len = rowptr_data[i + 1] - rowptr_data[i];
+          std::vector<scalar_t> shifted_prefix_sum(len,
+                                                   static_cast<scalar_t>(0));
 
-                       scalar_t cur = sum;
-#ifdef _OPENMP
-#pragma omp simd
-#endif
-                       for (size_t j = len; j > 0; j--) {
-                         cur -= beg[j - 1];
-                         out_beg[j - 1] = cur / sum;
-                       }
-                     }
-                   });
+          for (int64_t j = 1; j < len; ++j) {
+            shifted_prefix_sum[j] = shifted_prefix_sum[j - 1] + in_beg[j - 1];
+          }
+          scalar_t sum = shifted_prefix_sum[len - 1] + in_beg[len - 1];
+          scalar_t* pref_beg = shifted_prefix_sum.data();
+
+          int64_t d = 0;
+          for (; d < len - (len % Vec::size()); d += Vec::size()) {
+            Vec data_vec = Vec::loadu(pref_beg + d);
+            Vec data_vec2 = Vec(sum);
+            Vec output_vec = data_vec / data_vec2;
+            output_vec.store(out_beg + d);
+          }
+          if (len - d > 0) {
+            Vec data_vec = Vec::loadu(pref_beg + d, len - d);
+            Vec data_vec2 = Vec(sum);
+            Vec output_vec = data_vec / data_vec2;
+            output_vec.store(out_beg + d, len - d);
+          }
+        }
+      });
 }
 
 std::pair<at::Tensor, at::Tensor> biased_to_alias(at::Tensor rowptr,
